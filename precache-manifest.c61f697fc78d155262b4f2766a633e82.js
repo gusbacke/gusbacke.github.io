ec47d57637e2a49863330a030cf6dcd33626b590
gusbacke <gusbacke@student.gu.se>
2021-05-13 22:45:38 +0200

    Pushing a vue cli project

---
+++ precache-manifest.c61f697fc78d155262b4f2766a633e82.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3788739afdeedc9067f8",
+    "url": "/css/app.bd32afe8.css"
+  },
+  {
+    "revision": "f9ccf07c65d20c2ed38a",
+    "url": "/css/chunk-vendors.e2eba18d.css"
+  },
+  {
+    "revision": "0adf253e9b6a25d397bdc2f8cfb49dd6",
+    "url": "/img/quiz@192px.0adf253e.png"
+  },
+  {
+    "revision": "43989ae30fdbd4547b1464a356b51050",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a5c565dc894b525d612",
+    "url": "/js/about.af6b67af.js"
+  },
+  {
+    "revision": "3788739afdeedc9067f8",
+    "url": "/js/app.da0b2412.js"
+  },
+  {
+    "revision": "f9ccf07c65d20c2ed38a",
+    "url": "/js/chunk-vendors.8e54414d.js"
+  },
+  {
+    "revision": "22b8ba46a2c8a9dd76bb8b3aa77d484b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
